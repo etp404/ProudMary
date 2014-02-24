63aa5d46c79b330244323af9ed6b61e2b830500e
Matt Mould <matthew.mould@bbc.co.uk>
2014-02-24 09:49:46 +0000

    second commit

---
+++ src/main/com/matt/proudmary/listener/StartButtonListener.java
@@ -0,0 +1,17 @@
+package com.matt.proudmary.listener;
+
+import android.view.View;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mouldm02
+ * Date: 23/02/2014
+ * Time: 16:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class StartButtonListener implements View.OnClickListener {
+	@Override
+	public void onClick(View view) {
+
+	}
+}
---
+++ src/test/com/matt/proudmary/listener/StartButtonListenerTest.java
@@ -0,0 +1,20 @@
+package com.matt.proudmary.listener;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mouldm02
+ * Date: 23/02/2014
+ * Time: 16:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class StartButtonListenerTest {
+
+	@Test
+	public void testUpdateTextMethodUpdatesTest() {
+		assertEquals("a", "a");
+	}
+}
